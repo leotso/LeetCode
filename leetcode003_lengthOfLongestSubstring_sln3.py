909a74401302684b27a731d12b7ede526cd10cb8
leotso <leothao@gmail.com>
2017-01-13 14:25:34 +0800

    Optimized Sliding Window Method

---
+++ leetcode003_lengthOfLongestSubstring_sln3.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s:str
+        :rtype: int
+        """
+        n = len(s)
+        ans = 0
+        dic = {}
+        i = 0
+        for j in range(n):
+            if s[j] in dic:
+                i = max(dic[s[j]], i)
+            ans = max(ans, j - i + 1)
+            dic[s[j]] = j + 1
+            
+        return ans
+
+
+sln = Solution()
+testString = "testString"
+length = sln.lengthOfLongestSubstring(testString)
+print(length)
