d663b8a341f01178a0ff20598969114eaa72158a
leotso <leothao@gmail.com>
2017-01-16 23:24:36 +0800

    add O(log(m+n)) solution for problem Median of Two Sorted Arrays

---
+++ 004_MedianOfTwoSortedArrays_log(m+n).py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        l = (len(nums1) + len(nums2) + 1) >> 1
+        r = (len(nums1) + len(nums2) + 2) >> 1
+        return (self.getkth(nums1, nums2, l) + self.getkth(nums1, nums2, r)) / 2.0
+    
+    def getkth(self, nums1, nums2, k):
+        m = len(nums1)
+        n = len(nums2)
+        if m > n:
+            return self.getkth(nums2, nums1, k)
+        if m == 0:
+            return nums2[k - 1]
+        if k == 1:
+            return min(nums1[0], nums2[0])
+        
+        i = min(m, k / 2)
+        j = min(n, k / 2)
+        if nums1[i - 1] > nums2[j - 1]:
+            return self.getkth(nums1, nums2[j:], k - j)
+        else:
+            return self.getkth(nums1[i:], nums2, k - i)
+        return 0
+
+
+sln = Solution()
+nums1 = [1, 2, 5, 7, 8, 9]
+nums2 = [3, 4]
+print(sln.findMedianSortedArrays(nums1, nums2))
