233bb9a75f1181e568dc1b161297f1be133957b7
leotso <leothao@gmail.com>
2017-02-01 21:54:55 +0800

    add soulution one to problem Longest Common Prefix

--- 014_LongestCommonPrefix_sln1.py
+++ 014_LongestCommonPrefix_sln1.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type strs: List[str]
         :rtypr: str
         """
-        
+        if len(strs) == 0:
+            return ''
+        prefix = strs[0]
+        for s in strs:
+            while s.find(prefix) != 0:
+                prefix = prefix[0: len(prefix)-1]
+                if (prefix == ''):
+                    return ''
+        return prefix
+
+sln = Solution()
+ls = ['leets', 'leetcode', 'leet', 'leeds']
+print(sln.longestCommonPrefix(ls))
