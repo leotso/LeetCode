91892ae36da42a425043d3ee83ae1f39cf55ad5a
leotso <leothao@gmail.com>
2017-02-09 22:38:37 +0800

    add solution one to problem Three Sum Closest

---
+++ 016_ThreeSumClosest_sln1.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        result = nums[0] + nums[1] + nums[2]
+        for i in xrange(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum == target:
+                    return sum
+                if abs(sum - target) < abs(result - target):
+                    result = sum
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+        return result
+
+sln = Solution()
+s = [-1, 2, 1, -4]
+target = 1
+print(sln.threeSumClosest(s, target))
