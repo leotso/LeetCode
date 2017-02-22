35dc3816b9fb1f7cd9d78e5aeb333027c1f1672a
leotso <leothao@gmail.com>
2017-02-22 14:44:35 +0800

    modify solution one to problem Merge K Linked Lists

--- 023_MergeKSortedLists_sln1.py
+++ 023_MergeKSortedLists_sln1.py
@@ -10,4 +10,56 @@ class Solution(object):
         :type lists: List[ListNode]
         :rtype: ListNode
         """
+        while len(lists) > 1:
+            lists.append(self.mergeTwoLists(lists[0], lists[1]))
+            del lists[0:2]
+        return lists[0]
+
         
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if None in (l1, l2):
+            return l1 or l2
+        dummy = cur = ListNode(0)
+        dummy.next = l1
+        while l1 and l2:
+            if l1.val < l2.val:
+                l1 = l1.next
+            else:
+                nxt = cur.next
+                cur.next = l2
+                tmp = l2.next
+                l2.next = nxt
+                l2 = tmp
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
+
+# test
+def listToLinkList(l):
+    dummyHead = curr = ListNode(0)
+    for i in l:
+        curr.next = ListNode(i)
+        curr = curr.next
+    return dummyHead.next
+
+def printLinkList(listNode):
+    l = []
+    while True:
+        l.append(listNode.val)
+        if listNode.next == None:
+            break
+        listNode = listNode.next
+    print(l)
+
+lists = []
+lists.append(listToLinkList([1, 5, 8, 15]))
+lists.append(listToLinkList([3, 7, 13, 22]))
+lists.append(listToLinkList([2, 4, 6, 20]))
+lists.append(listToLinkList([4, 6, 19, 23]))
+sln = Solution()
+printLinkList(sln.mergeKLists(lists))
