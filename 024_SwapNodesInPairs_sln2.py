241a9f7f2460b0ade22d3c2fba9ab9bac80f8dd4
leotso <leothao@gmail.com>
2017-02-26 12:37:09 +0800

    add solution two of problem 024 SwapNodesInPairs

---
+++ 024_SwapNodesInPairs_sln2.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def swapPairs(sefl, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = curr = ListNode(0)
+        curr.next = head
+        while curr.next and curr.next.next:
+            a = curr.next
+            b = a.next
+            curr.next, b.next, a.next = b, a, b.next
+            curr = a
+        return dummy.next
+
+# test
+def listToLinkList(l):
+    dummyHead = curr = ListNode(0)
+    for i in l:
+        curr.next = ListNode(i)
+        curr = curr.next
+    return dummyHead.next
+
+def printLinkList(listNode):
+    l = []
+    while True:
+        l.append(listNode.val)
+        if listNode.next == None:
+            break
+        listNode = listNode.next
+    print(l)
+
+l1 = listToLinkList([1, 2, 3, 4])
+l2 = listToLinkList([1, 2, 3, 4, 5, 6, 7])
+sln = Solution()
+printLinkList(sln.swapPairs(l1))
+printLinkList(sln.swapPairs(l2))
